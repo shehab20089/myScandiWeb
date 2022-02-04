6946cd70381e18ba6b73e78e1b5027c525fc3829
shehab20089 <shehabmohsen1995@gmail.com>
2022-02-04 18:33:49 +0200

    adding checkout ovrride style

--- src/route/Checkout/Checkout.component.js
+++ src/route/Checkout/Checkout.component.js
@@ -1,6 +1,7 @@
 import { Checkout as SourceCheckout } from 'SourceRoute/Checkout/Checkout.component';
 import {BILLING_STEP,DETAILS_STEP,SHIPPING_STEP} from 'SourceRoute/Checkout/Checkout.config';
 import ContentWrapper from 'SourceComponent/ContentWrapper';
+import './Checkout.override.style.scss'
 
 
 class Checkout extends SourceCheckout {
@@ -10,32 +11,29 @@ class Checkout extends SourceCheckout {
         console.log(this.props.checkoutStep);
         
     }
-    // render (){
-    //     return (
-    //         <main block="Checkout">
-    //             <ContentWrapper
-    //               wrapperMix={ { block: 'Checkout', elem: 'Wrapper' } }
-    //               label={ __('Checkout page') }
-    //             >
-    //                 { this.renderSummary(true) }
-    //                 <div block="Checkout" elem="Step">
-    //                     { this.renderTitle() }
-    //                     { this.renderGuestForm() }
-    //                     { this.renderStep() }
-    //                     { this.renderLoader() }
-    //                 </div>
-    //                 <div>
-    //                     { this.renderSummary() }
-    //                     { this.renderPromo() }
-    //                     { this.renderCoupon() }
-    //                 </div>
-    //             </ContentWrapper>
-    //         </main>
-    //     );    }
-    //  renderStep(){
+    render (){
+        return (
+            <main block="Checkout">
+                <ContentWrapper
+                  wrapperMix={ { block: 'Checkout', elem: 'Wrapper' } }
+                  label={ __('Checkout page') }
+                >
+                    { this.renderSummary(true) }
+                    <div block="Checkout" elem="Step">
+                        { this.renderTitle() }
+                        { this.renderGuestForm() }
+                        { this.renderStep() }
+                        { this.renderLoader() }
+                    </div>
+                    <div>
+                        { this.renderSummary() }
+                        { this.renderPromo() }
+                        { this.renderCoupon() }
+                    </div>
+                </ContentWrapper>
+            </main>
+        );    }
 
-    //      return <div>"mohahah22"</div>
-    //  }
 
 }
 export default Checkout
